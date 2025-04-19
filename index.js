87ae1b8d3a62c5c6fab0925f22047166149b3831
palakurithiru <137908595+palakurithiru@users.noreply.github.com>
2025-04-19 23:41:10 +0100

    Files
    Files

---
+++ index.js
@@ -0,0 +1,9 @@
+// Random number generator game function
+function generateRandomNumber() {
+    const minNum = 1;
+    const maxNum = 100;
+    const number = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+    // Show the result in an alert box
+    alert(`The answer is ${number}.`);
+  }
+  
